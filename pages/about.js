b8556b076614d34a58aa7bdd31c1ecb8f28a9320
Naoki Endoh <naoki.endoh@gmail.com>
2017-05-23 11:38:52 +0900

    Add MyLayout component

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,7 @@
-import Header from '../components/Header'
+import Layout from '../components/MyLayout'
 
-export default () => (
-  <div>
-    <Header />
-    <p>This is the about page</p>
-  </div>
+const Page = () => (
+  <p>This is the about page</p>
 )
+
+export default () => (<Layout page={page}>)
